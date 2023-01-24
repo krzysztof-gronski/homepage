2a434e7d2863c6e1c26ad7e759d735e67f9b6113
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-01-24 14:00:45 +0100

    images path

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@
       textImgNumber = imgNumber > 2 ? extremeNumber : ++imgNumber;
     }
 
-    galleryItem.src = "./img/p" + textImgNumber + ".jpg";
+    galleryItem.src = "img/p" + textImgNumber + ".jpg";
   }
 
 }
