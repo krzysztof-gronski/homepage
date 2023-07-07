71585bd6a80cc0ad4de5ff0b1f2fe1e30e08c6fb
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-07-08 00:15:11 +0200

    cors test3

--- js/script.js
+++ js/script.js
@@ -30,10 +30,10 @@
   const footer = document.querySelector("footer");
 
   const header = document.querySelector("header");
-  header.innerHTML="test02";
+  header.innerHTML="test03";
 
   const fetchData = async () => {
-    const response = await fetch("https://127.0.0.1:3000/api/CORSTest");
+    const response = await fetch("http://127.0.0.1:3000/api/CORSTest");
     const data = await response.json();
     footer.innerHTML=data.name;
   };
